df4e1a7c585e8fd0621bf5ebd228cd1e464e8daf
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-04 10:47:40 +1000

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// primitive data types
+
+// let age = 20;
+// console.log(age);
+
+// yourAge = age;
+// console.log(yourAge);
+
+
+// age = 40;
+
+// console.log(age);
+// console.log(yourAge);
+
+// referential data types
+
+let person = {
+    age: 20,
+    name : "Ram",
+}
+
+console.log(person);
+
+person2 = person;
+
+console.log(person2);
+
+
+person.age = 40;
+person.name = "Hari";
+
+console.log(person);
+console.log(person2);
